484a0505b9729e3d46f2997f0fe08c77f38e00e0
K4RM4-KUN <jfuentesa@insjoaquimmir.cat>
2020-10-07 02:01:28 +0200

    calendari

--- calendari.php
+++ calendari.php
@@ -5,29 +5,15 @@ ini_set('display_errors', '0');
 $mesActual = getdate();#getdate() es una funcion que crea una array con disitintos datos sobre la fecha(año actual ,mes actual ,dia actual...), estos datos me ayudaran durante el programa para crear el calendario correctamente
 $monthDays = date(t)+1;#date(t) te da un valor numerico igual a la cantidad de dias de el mes actual
 $counter = 1;#esto es simplemente un contador para uno de los bucles
-$diaSemana=date("w",mktime(0,0,0,$mesActual[mday],1,$mesActual[year]));#Esta variable consigue el valor numerico igual al dia de la semana del primer dia de el mes actual, pasandole a mktime primero la hora(0,0,0) y despues el mes actual, luego 1 que se refiere al primer dia del mes y por utlimo el año actual
-switch ($diaSemana) {
-    case 0:
-        $diaSemana = $diaSemana+1;
+$diaSemana=date("w",mktime(0,0,0,$mesActual[month],1,$mesActual[year]));#Esta variable consigue el valor numerico igual al dia de la semana del primer dia de el mes actual, pasandole a mktime primero la hora(0,0,0) y despues el mes actual, luego 1 que se refiere al primer dia del mes y por utlimo el año actual
+
+switch ($diaSemana){ #Este switch determina el numero de espacios antes de el 
+	case 0:      #dia 1 del calendario
+    	$diaSemana=6;
         break;
-    case 1:
-        $diaSemana = $diaSemana+5;
-        break;
-    case 2:
-        $diaSemana = $diaSemana-2;
-        break;
-    case 3:
-        $diaSemana = $diaSemana-2;
-        break;
-    case 4:
-        $diaSemana = $diaSemana-2;
-        break;
-    case 5:
-        $diaSemana = $diaSemana-2;
-        break;
-    case 6:
-        $diaSemana = $diaSemana-2;
-        break;
+    default:
+    	$diaSemana-=1;
+
     }
 #Tabla
 echo('<table border="1" style="text-align:center;>');
